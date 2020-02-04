7fa7399961bb142fd978e935553235e6de8e0507
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-02-04 14:24:48 +0100

    fix configuration for CI

--- build.py
+++ build.py
@@ -4,6 +4,6 @@ from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder(None, False, False, "missing", None, None, options=["boost:header_only=True"])
+    builder = build_template_default.get_builder()
 
     builder.run()
